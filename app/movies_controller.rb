79dff6e04bea81575777a38390fad0514a29d309
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-23 17:21:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,8 +46,8 @@ def can_get_size_of_the_database
   Movie.all.size
 end
 
-def can_find_the_first_item_from_the_database_using_id(id)
-  movie = Movie.find_first_by_ID(id)
+def can_find_the_first_item_from_the_database_using_id
+  movie = Movie.find_first_by_ID
 end
 
 def can_find_by_multiple_attributes
